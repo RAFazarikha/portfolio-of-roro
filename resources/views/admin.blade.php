b41d8798101e1578314cf2e4d458d035f379f476
RAFazarikha <rafazarikha24@gmail.com>
2025-02-17 20:27:26 +0700

    feat: update admin page header and description for clarity and improved user experience

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -17,8 +17,8 @@
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
             <div class="text-center min-h-screen flex flex-col justify-center items-center">
-                <h1 class="text-6xl font-bold mb-8">Welcome to Edu-Course Manager</h1>
-                <p class="text-lg text-gray-400 mb-12">Your gateway to managing and exploring courses effectively.</p>
+                <h1 class="text-6xl font-bold mb-8">Welcome to Admin Side</h1>
+                <p class="text-lg text-gray-400 mb-12">Your way to manage your website more effectively.</p>
         
                 <div class="space-x-4">
                     <a href="{{ route('login') }}" class="px-8 py-3 bg-white text-black font-medium rounded-lg shadow-lg hover:bg-gray-200 transition">Login</a>
